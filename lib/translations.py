9b68351c6c7cf51a14c0eb86673baf816de4773d
Igor Chubin <igor@chub.in>
2017-04-06 16:39:57 +0200

    l10n: translated into 20 languages

---
+++ lib/translations.py
@@ -0,0 +1,25 @@
+# vim: encoding=utf-8
+
+LITS = {
+    'en': [ "New Moon",     "First Quarter",    "Full Moon",    "Last Quarter"      ],
+    'be': [ "Маладзік",     "Першая чвэрць",    "Поўня",        "Апошняя чвэрць"    ],
+    'bg': [ "Новолуние",    "Първа четвърт",    "Пълнолуние",   "Последна четвърт"  ],
+    'ca': [ "Noviluni",     "Quart creixent",   "Pleniluni",    "Lluna minvant"     ],
+    'cs': [ "Nov",          "První čtvrť",      "Úplněk",       "Poslední čtvrť"    ],
+    'da': [ "Nymåne",       "Tiltagende",       "Fuldmåne",     "Aftagende"         ],
+    'de': [ "Neumond",      "Erstes Viertel",   "Vollmond",     "Letztes Viertel"   ],
+    'es': [ "Luna nueva",   "Cuarto creciente", "Luna Llena",   "Cuarto menguante"  ],
+    'eo': [ "Novluno",      "Unua lunduono",    "Plenluno",     "Lasta lunduono"    ],
+    'fr': [ "Nouvelle lune","Premier quartier", "Pleine lune",  "Dernier quartier"  ],
+    'hr': [ "Mlađak",       "Prva četvrt",      "Uštap",        "Zadnja četvrt"     ],
+    'it': [ "Luna nuova",   "Primo quarto",     "Luna piena",   "Ultimo quarto"     ],
+    'ja': [ "新月",         "上弦",             "満月",         "下弦"              ],
+    'nl': [ "Nieuwe maan",  "Eerste kwartier",  "Volle maan",   "Laatste kwartier"  ],
+    'ru': [ "Новолуние",    "Первая четверть",  "Полнолуние",   "Последняя четверть"],
+    'pl': [ "Nów",          "Pierwsza kwadra",  "Pełnia",       "Ostatnia kwadra"   ],
+    'pt': [ "Lua nova",     "Quarto crescente", "Lua cheia",    "Quarto minguante"  ],
+    'ro': [ "Lună nouă",    "Primul pătrar",    "Lună plină",   "Ultimul pătrar"    ],
+    'sk': [ "Nov",          "Prvá štvrť",       "Úplnok",       "Posledná štvrť"    ],
+    'sr': [ "Mlađak",       "Prva četvrt",      "Uštap",        "Poslednja četvrt"  ],
+    'uk': [ "Молодик",      "Перша чверть",     "Повня",        "Остання чверть"    ],
+}
